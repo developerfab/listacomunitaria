a5eeaccbc307adfcefafc119a53f18fc09d52cb7
Cristian Fabricio Rodriguez Romero <fab7696650@hotmail.com>
2013-06-04 15:49:50 -0500

    Test de prueba sin funciones implementadas
    Se agregaron funciones para los test, probando las funcionalidades que deben tener los modelos, no se incluyen
    los atributos caracteristicos de los modelos.

--- generador/models.py
+++ generador/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    pass
+    pass 
 
 class Lista(models.Model):
     pass
--- generador/tests.py
+++ generador/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from generador.models import User
+from generador.models import User, Lista
 
 class TestUsuario(TestCase):
     def test_adicion_de_usuario(self):
@@ -7,10 +7,27 @@ class TestUsuario(TestCase):
         Este test se encarga de probar la adicion de un usuario al aplicativo web
         """
         usuario = User()
+        import pdb; pdb.set_trace()
         usuario.nombre = "cristian"
         usuario.nick = "fab48"
         usuario.correo = "fab7696650@hotmail.com"
         usuario.password = "123456"
+        usuario.save()
 
         logeo = User.objects.filter(nick="fab48")
         self.assertEqual(logeo[0].password, password)
+
+    def test_ingresar_lista(TestCase):
+        """
+        Esta test se encarga de probar la insercion de url en las listas de
+        reproduccion de cada usuario
+        """
+        lista = Lista()
+        lista.url = "http://www.youtube.com"
+        lista.id_usuario = "fab48"
+        lista.save()
+
+        canciones = Lista.objects.filter(id_usuario="fab48")
+        self.assertEqual(canciones.url,"http://www.youtube.com")
+
+
